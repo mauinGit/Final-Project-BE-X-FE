f4cea1f873c0c4201999618f384bb82104c2651b
Nabila Ayu Talita <nabilaayutalitaa@gmail.com>
2025-10-26 00:45:56 +0700

    feat(sidebarStudent): add animation open

--- frontend/src/components/students/SideBarStudent.jsx
+++ frontend/src/components/students/SideBarStudent.jsx
@@ -15,26 +15,26 @@ export default function SideBarStudent() {
     };
 
     return(
-        <nav className={`${open ? "w-72 p-5" : "w-36 p-10"} w-2xs bg-white h-screen justify-between relative ease-in-out p-10 pt-10 transition-all duration-300`}>
+        <nav className={`${open ? "w-72 p-5" : "w-28 p-10"} w-2xs flex flex-col bg-white h-screen justify-between relative ease-in-out px-8 pt-5 transition-all duration-300`}>
             <div className="flex flex-col gap-10">
-                <div className="flex flex-row gap-3 items-center">
+                <div className="flex gap-4 items-center cursor-pointer" onClick={() => setOpen(!open)}>
                     <img 
                         src="/assets/logo/logoGDC.svg" 
                         alt="Logo GDCourse" 
-                        className="w-15 h-12 sm:w-20 sm:h-18"
+                        className="w-15 h-12 sm:w-16 sm:h-14 transition-transform duration-300 hover:scale-110"
                     />
-                    <h1 className="text-heading text-3xl font-bold">GDCourse</h1>
+                    <h1 className={`text-heading text-3xl font-bold transition-all duration-300 ${!open && "hidden"}`}>GDCourse</h1>
                 </div>
-                <ul className="p-0">
-                    <li className="list-none block h-10 mb-2 relative">
+                <ul className={`flex flex-col transition-all duration-300 ${open ? "items-start" : "items-center"}`}>
+                    <li className="list-none block h-12 mb-2 relative">
                         <NavLink
                             to={"student/dashboard"}
-                            className={({ isActive }) => `group flex items-center h-full transition-all text-xl duration-200 ${isActive ? "text-heading " : "text-heading hover:text-blue hover:font-medium"}`}>
+                            className={({ isActive }) => `group flex items-center h-full transition-all text-xl duration-200 ${isActive ? "text-gray-500 " : "text-gray-500 hover:text-blue hover:font-medium"}`}>
                                 {({ isActive }) => (
                                     <div className="flex relative items-center w-full gap-4">
                                         <RxDashboard  
                                             size="24" 
-                                            className={`text-xl transition-colors ${isActive ? "text-blue" : "text-heading group-hover:text-blue"}`}
+                                            className={`text-xl transition-colors ${isActive ? "text-blue" : "text-gray-500 group-hover:text-blue"}`}
                                         />
                                         <span className={`${!open && "hidden"} transition-colors`}>Dashboard</span>
                                         {isActive && open && (
@@ -44,15 +44,15 @@ export default function SideBarStudent() {
                                 )}
                         </NavLink>
                     </li>
-                    <li className="list-none block h-10 mb-2 relative">
+                    <li className="list-none block h-12 mb-2 relative">
                         <NavLink
                             to={"/student/courseStudent"}
-                            className={({ isActive }) => `group flex items-center h-full transition-all text-xl duration-200 ${isActive ? "text-heading " : "text-heading hover:text-blue hover:font-medium"}`}>
+                            className={({ isActive }) => `group flex items-center h-full transition-all text-xl duration-200 ${isActive ? "text-gray-500 " : "text-gray-500 hover:text-blue hover:font-medium"}`}>
                                 {({ isActive }) => (
                                     <div className="flex relative items-center w-full gap-4">
                                         <GoBook  
                                             size="24" 
-                                            className={`text-xl transition-colors ${isActive ? "text-blue" : "text-heading group-hover:text-blue"}`}
+                                            className={`text-xl transition-colors ${isActive ? "text-blue" : "text-gray-500 group-hover:text-blue"}`}
                                         />
                                         <span className={`${!open && "hidden"} transition-colors`}>Courses</span>
                                         {isActive && open && (
@@ -62,15 +62,15 @@ export default function SideBarStudent() {
                                 )}
                         </NavLink>
                     </li>
-                    <li className="list-none block h-10 mb-2 relative">
+                    <li className="list-none block h-12 mb-2 relative">
                         <NavLink
                             to={"/student/myCourses"}
-                            className={({ isActive }) => `group flex items-center h-full transition-all text-xl duration-200 ${isActive ? "text-heading " : "text-heading hover:text-blue hover:font-medium"}`}>
+                            className={({ isActive }) => `group flex items-center h-full transition-all text-xl duration-200 ${isActive ? "text-gray-500 " : "text-gray-500 hover:text-blue hover:font-medium"}`}>
                                 {({ isActive }) => (
                                     <div className="flex relative items-center w-full gap-4">
                                         <BsCollection   
                                             size="24" 
-                                            className={`text-xl transition-colors ${isActive ? "text-blue" : "text-heading group-hover:text-blue"}`}
+                                            className={`text-xl transition-colors ${isActive ? "text-blue" : "text-gray-500 group-hover:text-blue"}`}
                                         />
                                         <span className={`${!open && "hidden"} transition-colors`}>My Courses</span>
                                         {isActive && open && (
@@ -84,9 +84,9 @@ export default function SideBarStudent() {
             </div>
             <button 
                 onClick={handleLogout}
-                className="flex items-center gap-4 text-heading hover:text-blue text-xl hover:font-medium transition-all duration-200 mb-6"
+                className={`flex items-center cursor-pointer gap-4 mb-4 text-gray-500 hover:text-blue text-xl hover:font-medium transition-all duration-300 ${open ? "items-start" : "items-center justify-center"}`}
             >
-                <IoLogOutOutline size="24"/>
+                <IoLogOutOutline size="26"/>
                 <span className={`${!open && "hidden"} transition-colors`}>Logout</span>
             </button>
         </nav>
